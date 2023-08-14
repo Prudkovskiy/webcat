d14b9215391e3bb991e7c329e7de3200dbbbb7b6
Nikolay Prudkovskiy <n.prudkovskiy@bi.zone>
2023-08-14 15:40:26 +0300

    airflow testing

---
+++ dags/k8s_test.py
@@ -0,0 +1,25 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
+    KubernetesPodOperator,
+)
+
+with DAG(
+    dag_id="example_k8s_operator",
+    schedule_interval="0 0 * * *",
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    dagrun_timeout=timedelta(minutes=60),
+) as dag:
+    k = KubernetesPodOperator(
+        name="hello",
+        image="ubuntu",
+        cmds=["bash", "-cx"],
+        arguments=["sleep 10"],
+        labels={"foo": "bar"},
+        task_id="dry_run_demo",
+        is_delte_operator_pod=False,
+        do_xcom_push=True,
+        # config_file='/'
+    )
